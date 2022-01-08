5941ebe44c0c1f59b1be49f715a230a0e855cac4
Daniel Kuiper <daniel.kuiper@live.nl>
2022-01-08 17:09:27 +0100

    reordering

--- app/wakkerdam/models/__init__.py
+++ app/wakkerdam/models/__init__.py
@@ -2,7 +2,6 @@ from app.wakkerdam.models.Actor import Actor, Wolf, Villager
 from app.wakkerdam.models.Article import Article
 from app.wakkerdam.models.Chat import Chat
 from app.wakkerdam.models.ChatLog import ChatLog
-from app.wakkerdam.models.ChatType import ChatType
 from app.wakkerdam.models.Chatter import Chatter
 from app.wakkerdam.models.Game import Game
 from app.wakkerdam.models.Invite import Invite
@@ -12,3 +11,4 @@ from app.wakkerdam.models.Player import Player
 
 from app.wakkerdam.models.constants.Localization import Localization
 from app.wakkerdam.models.constants.Character import Character
+from app.wakkerdam.models.constants.ChatType import ChatType
